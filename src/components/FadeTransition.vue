2fe2858e05187f0b304006cc523429ff28db8c2d
VincentBerdillon <vincent.berdillon@gmail.com>
2025-01-21 16:33:06 +0100

    feat/navTab

---
+++ src/components/FadeTransition.vue
@@ -0,0 +1,19 @@
+<template>
+    <Transition mode="out-in" name="fade">
+        <slot></slot>
+    </Transition>
+</template>
+
+<style>
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+</style>
