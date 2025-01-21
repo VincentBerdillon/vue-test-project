2af9c6bea8fa5457a0431e99b500f8678eb23262
VincentBerdillon <vincent.berdillon@gmail.com>
2025-01-21 18:27:35 +0100

    feat/composables

---
+++ src/composables/useSettings.ts
@@ -0,0 +1,24 @@
+import GeneralSettings from "@/components/GeneralSettings.vue";
+import { ref } from "vue";
+
+interface GeneralSettings {
+    username: string;
+    email: string;
+    about: string;
+    gender: string;
+    country: string;
+}
+
+const general = ref<GeneralSettings>({
+    username: '',
+    email:'',
+    about: '',
+    gender: 'male',
+    country: 'USA',
+})
+
+export function useSettings() {
+    return{
+        general
+    }
+}
