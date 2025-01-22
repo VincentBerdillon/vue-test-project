3595588f559b29f3ee4c15c3f24dfa6f522d8903
VincentBerdillon <vincent.berdillon@gmail.com>
2025-01-22 16:39:50 +0100

    feat/add-forms-types

--- src/composables/useSettings.ts
+++ src/composables/useSettings.ts
@@ -1,5 +1,12 @@
 import GeneralSettings from "@/components/GeneralSettings.vue";
-import { ref } from "vue";
+import { ref, watch } from "vue";
+
+interface SettingsMap {
+    general: GeneralSettings;
+    privacy: PrivacySettings;
+    notifications: NotificationsSettings;
+}
+type SettingsKey = keyof SettingsMap;
 
 interface GeneralSettings {
     username: string;
@@ -9,16 +16,59 @@ interface GeneralSettings {
     country: string;
 }
 
-const general = ref<GeneralSettings>({
+const init = <T extends SettingsKey>(key: T, defaults:SettingsMap[T]) => {
+    const stored = localStorage.getItem(key);
+    return stored != null
+    ? JSON.parse(stored)
+    : defaults;
+} 
+
+const watcher =
+<T extends SettingsKey>(key: T) =>
+(value: SettingsMap[T]) => {
+localStorage.setItem(key, JSON.stringify(value))}
+
+const general = ref<GeneralSettings>(
+    init('general',{
     username: '',
-    email:'',
+    email: '',
     about: '',
     gender: 'male',
-    country: 'USA',
-})
+    country: 'USA'}))
+
+watch(general, watcher('general'), {deep: true})
+
+interface NotificationsSettings {
+    email: boolean;
+    sms:boolean;
+}
+
+const notifications = ref<NotificationsSettings>(init
+    ('notifications', {
+    email: false,
+    sms: false,
+}))
+
+watch(notifications, watcher('notifications'), {deep: true})
+
+type Visibility = 'public' | 'private';
+
+interface PrivacySettings {
+    visibility: Visibility;
+    searchEngineIndexing: boolean;
+}
+
+const privacy = ref<PrivacySettings>(init('privacy',{
+    visibility: 'public',
+    searchEngineIndexing: false
+}));
+
+watch(privacy, watcher('privacy'), {deep: true})
 
 export function useSettings() {
     return{
-        general
+        general,
+        notifications,
+        privacy
     }
 }
